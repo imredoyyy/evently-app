40c6d04a8f301f6a5d2937dbf7e1914c953b7077
Redoy Saha <redoysaha3236@gmail.com>
2024-11-27 01:24:43 +0600

    feat(frontend): set-up tanstack-query

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -1,12 +1,48 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 5,
+      },
+    },
+  });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) {
+      browserQueryClient = makeQueryClient();
+    }
+    return browserQueryClient;
+  }
+}
 
 const Providers = ({
   children,
   ...props
 }: React.ComponentProps<typeof ThemeProvider>) => {
-  return <ThemeProvider {...props}>{children}</ThemeProvider>;
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom" />
+      <ThemeProvider {...props}>{children}</ThemeProvider>
+    </QueryClientProvider>
+  );
 };
 
 export default Providers;
