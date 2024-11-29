ea91d4452fae570c5dbe6d5775de7a278d361d0c
Redoy Saha <redoysaha3236@gmail.com>
2024-11-29 18:23:05 +0600

    add role based authoriazation in create event page

--- src/app/(protected)/create-event/page.tsx
+++ src/app/(protected)/create-event/page.tsx
@@ -1,21 +1,28 @@
+import { redirect } from "next/navigation";
 import {
   dehydrate,
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
 
-import { getCategories } from "@/actions/category.action";
-
 import { EventCreationForm } from "../components/event-form/event-creation-form";
 import Container from "@/components/layout/container";
 
+import { getCategories } from "@/actions/category.action";
+import { getSession } from "@/utils/get-session";
+
 const CreateEventPage = async () => {
+  const session = await getSession();
+
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
     queryKey: ["categories"],
     queryFn: getCategories,
   });
+
+  if (session?.user.role !== "host" && session?.user.role !== "admin")
+    redirect("/");
 
   return (
     <Container>
