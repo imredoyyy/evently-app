81d9ff005dfc9f982ab98f606ae0ddd9d0adcc81
Redoy Saha <redoysaha3236@gmail.com>
2024-12-01 00:25:22 +0600

    fix stripe webhook

--- src/app/api/webhook/stripe/route.ts
+++ src/app/api/webhook/stripe/route.ts
@@ -3,72 +3,79 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 import { stripe } from "@/lib/stripe";
-import type { CreateOrderParams, OrderItem } from "@/types";
+import type { CreateOrderParams } from "@/types";
 
 import { createOrder } from "@/actions/order.action";
 
 export const POST = async (req: Request) => {
   const body = await req.text();
-  const signature = (await headers()).get("Stripe-Signature") as string;
+  const sig = (await headers()).get("Stripe-Signature") as string;
+
+  let event: Stripe.Event;
+
   const webhookSecret = process.env.STRIPE_WEBHOOK;
 
   if (!webhookSecret) {
+    throw new Error("Missing Stripe webhook secret");
+  }
+
+  try {
+    event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
+  } catch (err) {
+    console.error("Stripe Webhook error: ", err);
     return NextResponse.json(
-      { error: "Stripe webhook secret not found" },
+      { error: `Webhook error: ${err || "Internal server error"}` },
       { status: 500 }
     );
   }
 
-  let event: Stripe.Event;
+  const session = event?.data?.object as Stripe.Checkout.Session;
 
-  try {
-    if (!signature) {
-      return NextResponse.json(
-        { error: "Missing Stripe signature" },
-        { status: 400 }
-      );
-    }
+  if (event.type === "checkout.session.completed") {
+    try {
+      // Get data from Stripe session
+      const { id, amount_total, payment_status, metadata, customer } = session;
+      console.log({ id, customer });
 
-    event = stripe.webhooks.constructEvent(body, signature, webhookSecret);
-
-    if (event.type === "checkout.session.completed") {
-      const session = event.data.object as Stripe.Checkout.Session;
-      const { metadata } = session;
-
-      if (!metadata?.userId || !metadata?.eventId || !metadata?.orderItems) {
+      if (!metadata?.userId || !metadata?.eventId || !metadata.orderItems) {
         return NextResponse.json(
           { error: "Missing required metadata" },
           { status: 400 }
         );
       }
 
+      // Prepare order date
       const orderData: CreateOrderParams = {
-        userId: metadata.userId,
         eventId: metadata.eventId,
-        orderItems: JSON.parse(metadata.orderItems) as OrderItem[],
-        totalAmount: session.amount_total
-          ? (session.amount_total / 100).toString()
-          : "0.00",
+        totalAmount: amount_total ? (amount_total / 100).toString() : "0.00",
+        userId: metadata.userId,
+        orderItems: JSON.parse(metadata.orderItems),
         status:
-          session.payment_status === "paid" ||
-          session.payment_status === "no_payment_required"
+          payment_status === "paid" || payment_status === "no_payment_required"
             ? "paid"
             : "pending",
       };
 
       const order = await createOrder(orderData);
 
-      return NextResponse.json({ success: true, order }, { status: 200 });
-    }
+      console.log("Order creared");
 
-    return NextResponse.json(
-      { success: false, error: `Unhandled Stripe event type: ${event.type}` },
-      { status: 400 }
-    );
-  } catch (error) {
-    return NextResponse.json(
-      { error: `Stripe webhook error ${error}` },
-      { status: 500 }
-    );
+      return NextResponse.json({ success: true, order }, { status: 200 });
+    } catch (err) {
+      console.error("Error creating order: ", err);
+      return NextResponse.json(
+        {
+          success: false,
+          error: "Error creating order: ",
+          err,
+        },
+        { status: 500 }
+      );
+    }
   }
+
+  return NextResponse.json(
+    { success: false, error: `Unhandled event ${event.type}` },
+    { status: 400 }
+  );
 };
