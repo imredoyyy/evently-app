a8ce079047ed5db2229706880025f50d4e5dd9bd
Redoy Saha <redoysaha3236@gmail.com>
2024-11-30 23:21:33 +0600

    refactor(frontend) - minor changes

--- src/components/shared/event-card.tsx
+++ src/components/shared/event-card.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import Link from "next/link";
-import { GlobeIcon, MapPinCheckIcon, TagIcon } from "lucide-react";
+import {
+  CalendarDaysIcon,
+  GlobeIcon,
+  MapPinCheckIcon,
+  TagIcon,
+} from "lucide-react";
 
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 
-import { PaginatedEventResponseType } from "@/actions/event.action";
+import { PaginatedEventResponseType } from "@/lib/db/queries/event.query";
+import { formatDateTime } from "@/utils/formatter";
 
 interface EventResponse {
   event: PaginatedEventResponseType["events"][number];
@@ -16,19 +22,19 @@ interface EventResponse {
 export const EventCard = ({ event }: EventResponse) => {
   return (
     <Card
-      style={{ gridRow: "span 4" }}
-      className="overflow-hidden max-w-[350px] w-full p-0 group grid rounded-xl h-full grid-rows-subgrid mx-auto shadow-xl shadow-muted/10"
+      style={{ gridRow: "span 5" }}
+      className="overflow-hidden max-w-[350px] w-full p-0 group grid rounded-xl h-full grid-rows-subgrid mx-auto shadow-md hover:shadow-xl transition-all duration-300"
     >
       <CardContent
-        style={{ gridRow: "span 4" }}
-        className="grid relative gap-4 p-4 grid-rows-subgrid"
+        style={{ gridRow: "span 5" }}
+        className="relative grid gap-4 p-4 grid-rows-subgrid"
       >
-        <Badge className="absolute top-2 left-6 z-10 px-4 py-1 font-normal hover:bg-primary capitalize">
-          <TagIcon className="size-4 shrink-0 mr-2" />
+        <Badge className="absolute z-10 px-4 py-1 font-normal capitalize top-2 left-6 hover:bg-primary">
+          <TagIcon className="mr-2 size-4 shrink-0" />
           <span>{event.categoryName}</span>
         </Badge>
 
-        <div className="overflow-hidden w-full rounded-lg">
+        <div className="w-full overflow-hidden rounded-lg">
           <Link href={`/events/${event.slug}`} title={event.title}>
             <Image
               src={event.image!}
@@ -52,10 +58,18 @@ export const EventCard = ({ event }: EventResponse) => {
           </p>
         </div>
 
-        <div className="flex flex-wrap justify-between items-center">
+        <div className="flex items-center gap-1 text-sm text-muted-foreground">
+          <CalendarDaysIcon className="shrink-0 size-4" />
+          <span>
+            {formatDateTime(event.startDateTime).dateOnly} -{" "}
+            {formatDateTime(event.endDateTime).dateOnly}
+          </span>
+        </div>
+
+        <div className="flex flex-wrap items-center justify-between">
           {event.isOnline ? (
-            <Badge className="gap-2 leading-none text-rose-500 bg-rose-500/15 hover:bg-rose-500/25 transition-colors">
-              <GlobeIcon className="size-4 mr-2 shrink-0" />
+            <Badge className="gap-2 leading-none transition-colors text-rose-500 bg-rose-500/15 hover:bg-rose-500/25">
+              <GlobeIcon className="mr-2 size-4 shrink-0" />
               <span>Online</span>
             </Badge>
           ) : (
@@ -66,7 +80,7 @@ export const EventCard = ({ event }: EventResponse) => {
           )}
         </div>
 
-        <div className="flex justify-between items-center">
+        <div className="flex items-center justify-between">
           <div className="space-y-1.5">
             <p className="text-sm text-muted-foreground">Organizer</p>
             <h3 className="font-medium leading-none">{event.organizerName}</h3>
